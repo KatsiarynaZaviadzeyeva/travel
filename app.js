e50aee2b0c9ff816dcff27b68f71e2b33dab2e91
KatsiarynaZaviadzeyeva <katsiarynaappleby@gmail.com>
2023-01-08 17:34:59 +0300

    feat: add burger

--- app.js
+++ app.js
@@ -8,3 +8,17 @@
         }
     };
 }());
+
+// Burger handler 
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__navigation');
+    const menuCloseItem = document.querySelector('.header__navigation-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__navigation_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__navigation_active');
+    });
+}());

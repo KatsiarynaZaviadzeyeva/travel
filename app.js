7724270fee913e175d087a39798d2373fddf2145
KatsiarynaZaviadzeyeva <katsiarynaappleby@gmail.com>
2023-01-09 20:34:50 +0300

    feat: add adaptability

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
     const header = document.querySelector('.header');
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
-            header.classList.add('header_active');
+            header.classList.add('header--active');
         } else {
-            header.classList.remove('header_active');
+            header.classList.remove('header--active');
         }
     };
 }());
@@ -16,9 +16,9 @@
     const menu = document.querySelector('.header__navigation');
     const menuCloseItem = document.querySelector('.header__navigation-close');
     burgerItem.addEventListener('click', () => {
-        menu.classList.add('header__navigation_active');
+        menu.classList.add('header__navigation--active');
     });
     menuCloseItem.addEventListener('click', () => {
-        menu.classList.remove('header__navigation_active');
+        menu.classList.remove('header__navigation--active');
     });
 }());
